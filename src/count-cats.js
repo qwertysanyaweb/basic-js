5a6af023bc2c0a5ff3598eac784e5aea2e78251a
qwertysanyaweb <qwerty.sanya.web@gmail.com>
2020-02-24 16:01:33 +0500

    Update countCats tfunction
    wrote a script

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,18 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(cat) {
+	var all = [];
+	var kol = cat.length;
+	if(kol < 1){return 0;}
+	for (var i = 0; i < kol; i++) {
+		for (var k = 0; k < cat[i].length; k++) {
+			all.push(cat[i][k])
+		}
+	}
+	var counts = {};
+	for(var i=0;i< all.length;i++)
+	{
+		var key = all[i];
+		counts[key] = (counts[key])? counts[key] + 1 : 1 ;
+	}
+	if(!counts['^^']){return 0;}
+	return counts['^^'];
 };
