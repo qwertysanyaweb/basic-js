4ed6bf7e4915799877fbf50cecb61b9e07b6236c
qwertysanyaweb <qwerty.sanya.web@gmail.com>
2020-02-24 16:02:17 +0500

    Update dreamteam
    Wrote a script

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+	if(!Array.isArray(arr) || !arr || arr == null || arr == undefined){
+		return false;
+	}
+	let word = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (typeof(arr[i]) != 'undefined' && typeof(arr[i]) != 'object' && typeof(arr[i]) != 'number' && arr[i] !== null && typeof(arr[i]) != 'boolean') {
+			if(!arr[i].replace( /\D/g, '')){
+				word.push(arr[i].trim().substr(0,1).toUpperCase());
+			}
+		}
+	}
+	let newword = word.sort();
+	let name = '';
+	for (var i = 0; i < newword.length; i++) {
+		name += word[i];
+	}
+	return name;
 };
