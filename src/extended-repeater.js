8c4d17f5638004ae1678dccc22d6597e1562dc40
qwertysanyaweb <qwerty.sanya.web@gmail.com>
2020-02-24 17:18:34 +0500

    Update extended-repeater.js
    wrote a script

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,42 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	var all = [];
+	var repeat = options['repeatTimes'];
+	if(repeat === undefined){
+		repeat = 1;
+	}
+	var separator = '+';
+	var addition = options['addition'];
+	var additionRepeatTimes = options['additionRepeatTimes'];
+	var additiontext = '';
+
+	if(!addition){
+		addition = '';
+	}
+
+	if(additionRepeatTimes > 1){
+		for(var i = 0; i < additionRepeatTimes; i++){
+			if(options['additionSeparator']){
+				additiontext += (options['additionSeparator'] + addition);
+			}else{
+				additiontext += addition;
+			}
+		}
+		addition = additiontext;
+	}
+
+
+	if(options['separator']){
+		separator = options['separator'];
+	}
+
+	for (var i = 0; i < repeat; i++) {
+		if(i == repeat - 1){
+			all += str + addition;
+		}else{
+			all += (str + addition + separator);
+		}
+
+	}
+
+	return all;
 };
-  
