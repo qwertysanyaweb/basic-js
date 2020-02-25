11c09e5ad1b04f82c257d69555ccb2cb7d558826
qwertysanyaweb <qwerty.sanya.web@gmail.com>
2020-02-25 16:50:15 +0500

    Update carbon-dating
    Wrote script

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+        let sampleActivityParsed = parseFloat(sampleActivity);
+        if (sampleActivityParsed > MODERN_ACTIVITY
+            || sampleActivityParsed <= 0
+            || (typeof sampleActivity != "string")
+            || !isFinite(sampleActivityParsed)) return false;
+
+      return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityParsed) / (0.693 / HALF_LIFE_PERIOD));
+    };
