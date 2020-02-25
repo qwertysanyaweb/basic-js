7ce0d939bc8e2a429c1e56d5aa193fb25748b640
qwertysanyaweb <qwerty.sanya.web@gmail.com>
2020-02-25 16:55:49 +0500

    Update transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,33 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+        if (!Array.isArray(arr)) throw Error;
+        if (arr.length === 0) return arr;
+
+        let newArr = [],
+            discardNextFlag = false;
+
+        for (let index = 0; index < arr.length; index++) {
+            if (discardNextFlag) {
+                discardNextFlag = false;
+                continue;
+            }
+
+            switch (arr[index]) {
+                case '--discard-next':
+                    discardNextFlag = true;
+                    break;
+                case '--discard-prev':
+                    index === 0 || newArr.pop();
+                    break;
+                case '--double-next':
+                    index >= arr.length - 1 || newArr.push(arr[index + 1]);
+                    break;
+                case '--double-prev':
+                    if (index === 0) break;
+                    newArr.push(arr[index-1]);
+                    break;
+                default:
+                    newArr.push(arr[index]);
+            }
+        }
+        return newArr
+    };
